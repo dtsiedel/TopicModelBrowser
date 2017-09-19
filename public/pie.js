484923abe03417eb9811ee60af77e40e4f524675
Drew Siedel <dts.siedel@gmail.com>
2017-09-19 18:27:32 -0400

    Bar/Donut Working, Last Commit on D3V4

--- public/pie.js
+++ public/pie.js
@@ -1,9 +1,10 @@
+
 function main()
 {
     //need some data to represent
     var data = [{name: "one", value: 430, color:"#391029"},
                 {name: "two", value: 500, color:"#FEABCE"},
-                {name: "three", value: 200, color:"#123ABC"},
+                {name: "three", value: 250, color:"#123ABC"},
                 {name: "four", value: 400, color:"#ADDBAD"},
                 {name: "five", value: 600, color:"#FADCAB"},
                 {name: "six", value: 350, color:"#AFE421"}];
@@ -13,6 +14,17 @@ function main()
     var width = 400 - margin.left - margin.right;
     var height = width - margin.top - margin.bottom;
     var radius = Math.min(width, height) / 2;
+
+    function arcTween(d) {
+      arc = d3.arc().outerRadius(radius*1.1).innerRadius(radius-50).cornerRadius(5);
+      return arc(d);
+    }
+
+    function donutTween(b) {
+      b.outerRadius= radius * 1.2;
+      var i = d3.interpolate({outerRadius: 0}, b);
+      return function(t) { return arc(i(t)); };
+    }
     
     // add the canvas to the DOM 
     var chart = d3.select("#pie-demo")
@@ -26,7 +38,7 @@ function main()
         .outerRadius(radius)
         .innerRadius(radius - 50)
         .cornerRadius(5);
-
+    
     var pie = d3.pie()
         .sort(null)
         .startAngle(Math.PI)
@@ -36,7 +48,17 @@ function main()
     var g = chart.selectAll(".arc")
         .data(pie(data))
         .enter().append("g")
-        .attr("class", "arc"); 
+        .attr("class", "arc")
+        ;
+        /*
+        .on("mouseover", function(d){
+            console.log("on");
+            d3.select(d).transition().duration(250).attrTween("d",donutTween);
+        })
+        .on("mouseout", function(){
+            console.log("off")
+        });
+        */
 
     g.append("path")
       .style("fill", function(d) { return d.data.color; })
@@ -58,6 +80,8 @@ function main()
                         .attr("class", "monthText")
                         .attr("x", 7) //Move the text from the start angle of the arc
                         .attr("dy", 18) //Move the text down
+                        .attr("letter-spacing", "1px")
+                        .style("font-family", "sans-serif")
                    .append("textPath")
                         .attr("xlink:href",function(d,i){return "#arc_"+i;})
                         .text(function(d){return "Topic " + d.name;})
