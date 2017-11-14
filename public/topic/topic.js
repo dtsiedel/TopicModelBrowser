ff0bbabc0f6625e617506f8b274b7304398924fb
Drew Siedel <dts.siedel@gmail.com>
2017-11-14 00:36:44 -0500

    Add Alex's Unary Topic Files

---
+++ public/topic/topic.js
@@ -0,0 +1,245 @@
+// Alex Van Heest
+// Single Topic View
+
+// Designed to display unary topic view of data.
+// Meant to show a single topic through excerpts from a series of documents.
+
+// Sample data:
+var example_title = "Light It Up Blue isn’t autism awareness: it’s Autism Speaks advertisement";
+var example_link = "https://leftbrainrightbrain.co.uk/2017/04/02/light-it-up-blue-isnt-autism-awareness-its-autism-speaks-advertisement/"
+var example_paragraph = "What do these autism organizations have in common: Autism Science Foundation Autistic Self Advocacy Network Autism Society of America National Autistic Society Autistica The Marcus Autism Center The Thinking Person\u2019s Guide to Autism I could list many more.   Besides being autism organizations, they all have this in common: no mention of \u201cLight It Up Blue\u201d on their webpages today. Today is Autism Acceptance Day (aka Autism Awareness Day).  Autism Speaks has been pushing this day with their \u201cLight It Up Blue\u201d campaign.  Autism Speaks describes Light It Up Blue as  Each April 2nd, Autism Speaks celebrates the start of its signature campaign, Light It Up Blue, along with the international autism community in recognition of UN sanctioned World Autism Awareness Day and April, World Autism Month. They are clear\u2013it\u2019s not an autism event, it\u2019s an  Autism Speaks  event. Apparently \u201calong with the international autism community\u201d.  Which doesn\u2019t appear to be joining in. Blue, by the way, is not the color of autism (it doesn\u2019t really have one).  Blue is the color of Autism Speaks. Want to do something for the autism community today?  Take a look at The Thinking Person\u2019s Guide to Autism\u2019s article:  Autism Acceptance Day & Month: Do This, Not That \u2014 By Matt Carey \n\t\t\tdiv.wpmrec2x{max-width:610px;}\n\t\t\tdiv.wpmrec2x div.u > div{float:left;margin-right:10px;}\n\t\t\tdiv.wpmrec2x div.u > div:nth-child(3n){margin-right:0px;}";
+
+var long_example_title = "An open letter to William Shatner on autism awareness";
+var long_example_link = "https://leftbrainrightbrain.co.uk/2017/04/03/an-open-letter-to-william-shatner-on-autism-awareness/";
+var long_example = "Mr. Shatner, I see that you have been involved in a rather large tweet storm this weekend, focusing on your support for Autism Speaks. My guess is you would agree that 140 characters at a time is far too limiting to take on a complex discussion. Open letters such as this are as well, but at least I can go into a bit more detail. I hope you take the time to read and at least try to see at least my side of this discussion. First off, yes, I am part of the autism communities. My kid is autistic. I try to guard my kid’s privacy so I don’t give out a lot of details. But let me just say this: if people tell you, “Obviously his kid is ‘high functioning’ so he doesn’t understand what ‘real’ autism is about”, they are wrong. Let me add: anyone who takes the position of discounting another’s voice based on some measure of “severity” of autism is harming our communities. My kid’s challenges are very different, but no less real than those of self-advocates you are hearing on twitter. Just as my kid’s challenges are very different, but no less real, than those of the other kids at his school who will likely never be on twitter. Another way to put this is this: people will likely tell you this is a divide between parents of kids with “real” autism vs. self-advocates with “mild” autism. For what it is worth, some of my kid’s strongest allies are those self-advocates you are hearing from. Some of those who have done the most damage are other parents who, well meaning as they are, have increased the stigma of autism. I haven’t seen that misconception (“real” vs “mild” autism) in your tweets (I haven’t read them all, but I haven’t seen it), but I have seen others. For example: They apparently don't want awareness so they attack (under the guise of educating) those that supported the awareness day. https://t.co/EuUOIVtI4p — William Shatner (@WilliamShatner) April 3, 2017 //platform.twitter.com/widgets.js There is a difference, a big difference, between autism awareness and promoting Autism Speaks. “Light it Up Blue” isn’t an autism community effort, it is an Autism Speaks effort. Most autism organizations don’t promote “light it up blue”. I didn’t find one in my search yesterday other than Autism Speaks. There probably were some, but major organizations were not a part of “light it up blue”. This is why people focused on the avatar you used on Twitter yesterday. It was an Autism Speaks “light it up blue” logo. The blue light bulb logo with the Autsim Speaks puzzle piece. So, why would supporting Autism Speaks spark a strong response? I bet there are as many answers (more even) that tweets directed at you. But let me tell you about a few reasons. First on the list for me is the fact that over the years Autism Speaks has used truly bad depictions of autistics in their promotional materials. Their “I Am Autism” video is an example of this. It’s the sort of inspiration at the expense of the disabled that we saw in generations past. It presents autism as a monster that steals children. It presents the autism community as all the allies, but not the autistics. Seriously, read the transcript on who the community is. You have not properly been introduced to this community of parents and grandparents, of siblings and friends and schoolteachers and therapists and pediatricians and scientists. This is a systemic problem in the autism community: considering it a community of the allies, not the autistics. The I am Autism video is old, and I will admit that Autism Speaks has evolved since then. But they aren’t where we need them to be yet. I saw that people tweeted to you about how Mr. John Robison, an autistic self-advocate, left Autism Speaks. He was at the time the first and only autistic in an important position in Autism Speaks. Please take the time to read his article (in the link I just gave) about his reasons for leaving. Analogies are always flawed, and coming up with a good one here is difficult. But imagine that the NAACP were staffed by well meaning white parents who had adopted African American kids. That African American’s themselves didn’t have a voice in their own advocacy organization. That they didn’t listen when an African American voiced a different opinion than theirs. Please don’t fall for the false dichotomy of whether it should be parents or self-advocates who direct autism advocacy. It should be both. There will always be parents and siblings and others who hold the place at the table for autistics who can’t be there themselves (by the way, this includes many self-advocates. But that is another discussion). But we as parents have to work with the spectrum of our community, or we are only advocates for our own child not the whole. We need to work with adult and non-adult self-advocates. Not in spite of our differences in focus, but because of our difference in focus. I have seen you respond to people that, instead of tweeting at you, perhaps they should focus their attention on organizing their own advocacy efforts. Many have. By the way, tweeting one’s opposition to your actions is advocacy. That said, one of the issues with Autism Speaks is that they have corportatized autism advocacy, branded themselves as the one-stop autism adovcacy location, to the point that local dollars are not available for small, community based advocacy efforts. And they have the power to get off their butts & create or support an organization that aligns with them instead of harassing others. https://t.co/9YYuexUAIv — William Shatner (@WilliamShatner) April 2, 2017 //platform.twitter.com/widgets.js Case in point: you chose the Autism Speaks logo, the Autism Speaks motto (light it up blue), as being a generic autism advocacy effort. Again, this is a big reason why that logo, that icon, garnered the reaction you received. Lastly, let me point out that over its history Autism Speaks has promoted the failed and damaging “autism is a vaccine epidemic” idea. Again, they have evolved over time. But they still have far to go. The vaccines-cause-autism idea is so incredibly damaging to our communities, to autistics, that I could easily triple the length of this piece by discussing it. Let me try to be brief. First off, it’s wrong. Simply and clearly, it’s wrong. You will find parents who believe it, who promote the idea. That doesn’t make it correct. Listen to the parents who promote the vaccines-cause-autism idea. Hear and feel their pain. And ask yourself, if you could help them to not feel that pain, wouldn’t you? If what you are doing is causing more people to feel that pain, wouldn’t you want to change? The path to stopping that pain is by getting good information out. Accuate information. Vaccines do not cause autism. You probably woudn’t believe me if I told you all the fake “cures” sold to autism parents under the guise of “healing vaccine injury”. Chemical castration. Bleach drinks and enemas. Those are but two of the more abusive. There are many more, fake cures which tend to have one thing in common (in addition to being worthless): costing families lots of money. But if you say your “treatment” is for “vaccine injury”, no-one in the vaccines-cause-autism community will speak out against you. That’s where Autism Speaks could and should step up. Could and should make a difference for our communities. Some have tried, but as an organization, this has been a spectacular failure for Autism Speaks. Rob Ring, Chief Science Officer of Autism Speaks (which touts itself as a science based organization) was very clear about this. He made the statement below on the Autism Speaks website: Over the last two decades, extensive research has asked whether there is any link between childhood vaccinations and autism. The results of this research are clear: Vaccines do not cause autism. We urge that all children be fully vaccinated. Rob Ring Here’s the thing–Bob Wright vetoed that message. First he put his own message up together with Dr. Ring’s. Then he disappeared Ring’s message. I hope you took the time to read this. I hope you can take the time to understand the positions here. I don’t ask you to agree, but to understand. I don’t see that so far. Understanding–it’s a form of acceptance. Understand the positions of the people who disagree with you. Accept that they have a different point of view. Even if they are sometimes harsh. By Matt Carey";
+
+var example_wordlist = ["autism", "Light", "Blue", "foundation"];
+//var example_wordlist = ["div:nth-child(3n){margin-right:0px;}"];
+
+
+// Finds most exemplary sentence from article for a given topic. Annotates as it processes each sentence, returns
+// an output that includes three sentence-excerpt from the article.
+function find_best_excerpt_from_selection(title, doc_number, percent_similarity, original_article, wordlist, link)
+{
+    var split_list = original_article.split(/[\n\t\r.]/);
+    //var split_list = split(str);    // TB added sentence-splitter NPM module
+    var kw_dict = {};
+
+    for (var k = 0; k < wordlist.length; k++)
+    {
+        kw_dict[wordlist[k]] = wordlist.length - k;
+    }
+
+    //var best_total_unique_wordcount = 0;
+    var best_total_wordcount = 0;
+    var best_sentence_index = -1;
+    var all_annotated_sentences = [];
+
+    //var current_total_unique_wordcount = 0;
+    var current_total_wordcount = 0;
+
+    // loop through the whole string, sentence by sentence
+    for (var i = 0; i < split_list.length; i++)
+    {
+        // setup new annotations index
+        all_annotated_sentences.push("");
+
+        //current_total_unique_wordcount = 0;
+        current_total_wordcount = 0;
+
+        // split element split_list[i] of words into another array
+        //var current_sentence_array = split_list[i].split(" ");
+        var temp_par = split_list[i];
+
+        var ea_word_in_current_sentence = split_list[i].split(" ");
+        // loop through each word in the current sentence
+        for (var j = 0; j < ea_word_in_current_sentence.length; j++)
+        {
+            var current_word_score = kw_dict[ea_word_in_current_sentence[j].toLowerCase()];
+            if (current_word_score != null) // if current word is a keyword
+            {
+                current_total_wordcount += 1;
+
+                // annotation:
+                all_annotated_sentences[i] += "<span style=\"background-color: #FFFF00\">";
+                all_annotated_sentences[i] += ea_word_in_current_sentence[j];
+                all_annotated_sentences[i] += "</span> ";
+            }
+            else
+            {
+                // no annotation:
+                all_annotated_sentences[i] += ea_word_in_current_sentence[j];
+            }
+
+            if (j + 1 < ea_word_in_current_sentence.length)
+            {
+                all_annotated_sentences[i] += " ";
+            }
+        }
+        all_annotated_sentences[i] += ". ";
+
+        if (current_total_wordcount > best_total_wordcount)
+        {
+            best_total_wordcount = current_total_wordcount;
+            best_sentence_index = i;
+        }
+
+
+    }
+
+    // what we are left with is the index of the best fitting example for this topic, let's output it
+    if (best_sentence_index == -1)  // this would mean nothing was found with > 0 unique / total words: handle with
+                                    // explanatory output to HTML (note: in long term, we'd just ignore this case)
+    {
+        document.getElementById("sample").innerHTML += "<p>Error: no keyword matches found!</p>"
+        return
+    }
+
+    // === START OLD DEBUG OUTPUT ===
+    // document.getElementById("sample").innerHTML += "<h3>Best Fitting Sentence:</h3>";
+    // document.getElementById("sample").innerHTML += all_annotated_sentences[best_sentence_index] + "<br/>";
+    // document.getElementById("sample").innerHTML += "<h3>Total Keyword Matches:</h3>";
+    // document.getElementById("sample").innerHTML += best_total_wordcount.toString() + "<br/>";
+    // document.getElementById("sample").innerHTML += "<h3>Topic Word List:</h3><ul>";
+    // for (var m = 0; m < wordlist.length; m++)
+    // {
+    //     document.getElementById("sample").innerHTML += "<li>" + wordlist[m] + "</li>";
+    // }
+    // document.getElementById("sample").innerHTML += "</ul>";
+    //document.getElementById("sample").innerHTML += "<h3>Resulting Excerpt</h3>";
+    // === END OLD DEBUG OUTPUT ===
+
+    var first = -1, second = -1, last = -1;
+    // When we present the resulting excerpt, we want to show three sentences no matter where the excerpt is.
+    if (best_sentence_index == split_list.length - 1)   // means it's first/last sentence
+    {
+        //console.log("Found last sentence is best fit.");
+        first = split_list.length - 3;
+        second = split_list.length - 2;
+        last = split_list.length - 1;
+    }
+    else if (best_sentence_index == 0)   // means it's first/last sentence
+    {
+        //console.log("Found first sentence is best fit.");
+        first = 0;
+        second = 1;
+        last = 2;
+    }
+    else
+    {
+        //console.log("Found middle sentence is best fit.");
+        first = best_sentence_index - 1;
+        second = best_sentence_index;
+        last = best_sentence_index + 1;
+    }
+
+    // Edit display:
+    document.getElementById("sample").innerHTML += "<br><br>";
+    document.getElementById("sample").innerHTML += "<h3><a href='#document:" + doc_number.toString() + "'>" + title
+        + "</a>  <a href='" + link + "'> [original article] </a></h3>";
+    document.getElementById("sample").innerHTML += "<h4>" + percent_similarity.toString() + "% topic match</h4>";
+    document.getElementById("sample").innerHTML += all_annotated_sentences[first];
+    document.getElementById("sample").innerHTML += all_annotated_sentences[second];
+    document.getElementById("sample").innerHTML += all_annotated_sentences[last];
+}
+
+var timer = function(name) {
+    var start = new Date();
+    return {
+        stop: function() {
+            var end  = new Date();
+            var time = end.getTime() - start.getTime();
+            //console.log('Timer:', name, 'finished in', time, 'ms');
+            return time;
+        }
+    }
+};
+
+// Benchmarking method. Takes a number of iterations as input and outputs results to console.
+function benchmarking_excerpt_selector(iterations)
+{
+    var times_list = [];
+    console.log("Starting now");
+    for (var i = 0; i < iterations; i++)
+    {
+        var t = timer(i.toString(), times_list);
+        find_best_excerpt_from_selection(long_example_title, long_example);
+        times_list.push(t.stop());
+
+        // print an update every 100 iterations
+        if (i % 100 === 0)
+        {
+            console.log("Just hit iteration " + i.toString());
+            document.getElementById("sample").innerHTML = "";   // clear display; this will eat up lots of time to fill
+        }
+    }
+    document.getElementById("sample").innerHTML = "";
+    // print results from times_list
+    var total_time = 0;
+    for (var i = 0; i < times_list.length; i++)
+    {
+        total_time += times_list[i];
+    }
+    var average_time = total_time/iterations;
+    console.log("TOTAL TIME: " + total_time.toString());
+    console.log("AVERAGE TIME: " + average_time.toString());
+}
+
+// Given topic number, sets H2 in HTML to "TOPIC ###"
+function define_topicname_from_url()
+{
+    var original_url = window.location.href;
+    var topic_number_loc = original_url.indexOf("topic:");
+    var topic_number = original_url.substr(topic_number_loc+6);
+    document.getElementById("topicname").innerHTML = "Topic " + topic_number.toString();
+}
+
+
+define_topicname_from_url();
+
+var doc_number = 14;
+
+find_best_excerpt_from_selection(example_title, doc_number, 69, example_paragraph, example_wordlist, example_link);
+find_best_excerpt_from_selection(long_example_title, doc_number+1, 69, long_example, example_wordlist, long_example_link);
+
+
+// =================================================================
+// After 10,000 iterations @ 1,440 words / article:
+// TOTAL TIME: 252699
+// AVERAGE TIME: 25.2699 ms / document
+// =================================================================
+// After 1,000 iterations @ 1,440 words / article:
+// TOTAL TIME: 22012
+// AVERAGE TIME: 22.012 ms / document
+// =================================================================
+// After 100 iterations @ 1,440 words / article:
+// TOTAL TIME: 2399
+// AVERAGE TIME: 23.99 ms / document
+// =================================================================
+// EXPECTED RUNTIME FOR ONE PAGE OF LONG DOCUMENTS: ~24*20 = 480ms
+// =================================================================
+
+
+// TODO FOR 11/13:
+// X. Annotations part after this; brightest highlight / bold / size for most common words
+// 2. Review how to sort documents by how much they adhere to a topic, then run this search on that order.
+
+// TODO FOR 11/15:
+// X. Add links to document titles.
+// X. Finish formatting for document titles.
+// 3. Add pages for all documents above a certain threshold; start with five w/ 20 docs / page, then move to infinite.
+
+// TODO FOR 11/20:
+// 1. Sentence splitter API.
+// 2. Add more than just top 20 results; add an infinite scroll above 100.
+
+// TODO CONSIDERATIONS:
+// 1. If runtime is an issue, run as much server-side as possible
+
+
+// Design
+// ultimately infinite scroll above threshold (ie: 1% match)
+// display # sentences is proportional to % match
+// split into pages
+// display topic title not title (n many characters)
+// review how to load text
