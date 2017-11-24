663cb02b3b456c28b45b29f7d309b7741aa0b3ee
Drew Siedel <dts.siedel@gmail.com>
2017-11-23 20:59:51 -0500

    Change constant

--- public/spectrum.js
+++ public/spectrum.js
@@ -40,7 +40,7 @@ function constructSpectrum(t1, t2)
 {
     var color_scale = d3.interpolateRgb(colors[t1], colors[t2]);
     var shared = ribbon_data[t1][t2];
-    shared = shared.slice(0,500);
+    shared = shared.slice(0,250);
    
     getDocumentData(shared, function()
     { 
